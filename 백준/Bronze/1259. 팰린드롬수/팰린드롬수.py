2fd4a1f29c1fb9de1e508a10882d697c032618d8
Kim HyunJoon <72384046+hyun7494@users.noreply.github.com>
2024-03-13 19:57:30 +0900

    Update 팰린드롬수.py

--- 백준/Bronze/1259. 팰린드롬수/팰린드롬수.py
+++ 백준/Bronze/1259. 팰린드롬수/팰린드롬수.py
@@ -1,30 +1,43 @@
+# while True:
+#     N = input()
+#     if N == "0":
+#         break
+#     else:
+#         n = list(N)
+#         # print(n)
+#         # print(len(n))
+#         if len(n) == 1:
+#             print('yes')
+#         elif len(n) == 3:
+#             if n[0] == n[2]:
+#                 print("yes")
+#             else:
+#                 print("no")
+#         elif len(n) == 5:
+#             if n[0] == n[4] and n[1] == n[3]:
+#                 print("yes")
+#             else:
+#                 print("no")
+#         if len(n) == 2:
+#             if n[0] == n[1]:
+#                 print("yes")
+#             else:
+#                 print("no")
+#         elif len(n) == 4:
+#             if n[0] == n[3] and n[1] == n[2]:
+#                 print("yes")
+#             else:
+#                 print("no")
+# 쌩 노가다 했는데 파이썬은 다른방법 있음
 while True:
-    N = input()
-    if N == "0":
+    input_num = input()
+    
+    if input_num == "0":
         break
-    else:
-        n = list(N)
-        # print(n)
-        # print(len(n))
-        if len(n) == 1:
-            print('yes')
-        elif len(n) == 3:
-            if n[0] == n[2]:
-                print("yes")
-            else:
-                print("no")
-        elif len(n) == 5:
-            if n[0] == n[4] and n[1] == n[3]:
-                print("yes")
-            else:
-                print("no")
-        if len(n) == 2:
-            if n[0] == n[1]:
-                print("yes")
-            else:
-                print("no")
-        elif len(n) == 4:
-            if n[0] == n[3] and n[1] == n[2]:
-                print("yes")
-            else:
-                print("no")
+    
+    answer = "no"
+
+    if input_num == input_num[::-1]:
+        answer = "yes"
+    
+    print(answer)
