221bd9e9efc607c0e3fe54a7b6a8648aa57c7792
Kim HyunJoon <72384046+hyun7494@users.noreply.github.com>
2024-10-22 15:54:44 +0900

    [level 0] Title: 점의 위치 구하기, Time: 0.03 ms, Memory: 72.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/120841. 점의 위치 구하기/점의 위치 구하기.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int solution(int[] dot) {
+        int result = 0;
+
+        if (dot[0] > 0 && dot[1] > 0) {
+            result = 1;
+        }
+        else if (dot[0] > 0 && dot[1] < 0) {
+            result =  4;
+        }
+        else if (dot[0] < 0 && dot[1] < 0) {
+            result = 3;
+        }
+        else if (dot[0] < 0 && dot[1] > 0) {
+            result =  2;
+        }
+        int answer = result;
+        return answer;
+    }
+}
