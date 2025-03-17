35c9ed595bdf80cd17ec080057f87b87ecac9579
Kim HyunJoon <72384046+hyun7494@users.noreply.github.com>
2025-03-17 21:45:11 +0900

    [Silver I] Title: IOIOI, Time: 240 ms, Memory: 22068 KB, Score: 100 point -BaekjoonHub

---
+++ 백준/Silver/5525. IOIOI/IOIOI.java
@@ -0,0 +1,37 @@
+import java.io.IOException;
+import java.io.*;
+import java.math.BigInteger;
+import java.util.StringTokenizer;
+import java.util.*;
+
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int M = Integer.parseInt(br.readLine());
+        char s[] = br.readLine().toCharArray();
+
+        int result = 0;
+        int count = 0;
+
+        for(int i=1; i < M - 1; i++) {
+            if(s[i - 1] == 'I' && s[i] == 'O' && s[i + 1] == 'I') {
+                count++;
+
+                if(count == N) {
+                    count--;
+                    result++;
+                }
+                i++;
+            }
+            else {
+                count = 0;
+            }
+        }
+
+        System.out.println(result);
+
+    }
+}
